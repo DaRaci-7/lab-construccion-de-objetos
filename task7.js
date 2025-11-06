38c3699becbae43319bb0e7aa6f6f1af71d913c4
Ariel Norambuena <noramburiel@gmail.com>
2025-11-06 13:31:17 -0300

    feat(task7.js): add car constructor

--- task7.js
+++ task7.js
@@ -1,8 +1,17 @@
 
 
 // Type your code below this line!
+function Car(make, model, year, engine, mileage) {
+    this.make = make,
+    this.model = model,
+    this.year = year,
+    this.engine = engine,
+    this.mileage = mileage
+}
 
+const newCar = new Car("Hyundai", "Creta Su2id", 2025, "a combustión", "0km")
 
+console.log("El auto es un " + newCar.make + " " + newCar.model + " del año " + newCar.year + ", con un motor " + newCar.engine + " y un kilometraje de " + newCar.mileage);
 
 // Type your code above this line!
 
