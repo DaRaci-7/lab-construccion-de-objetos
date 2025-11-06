5128c74dc681bbe27d44d15a4e06e0906bf64bd3
Ariel Damián Norambuena <noramburiel@gmail.com>
2025-11-06 16:14:02 -0300

    feat(Task7): Adds prompt function to allow user interaction

--- task7.js
+++ task7.js
@@ -1,6 +1,8 @@
 
 
 // Type your code below this line!
+import promptSync from "prompt-sync";
+const prompt = promptSync();
 function Car(make, model, year, engine, mileage) {
     this.make = make,
     this.model = model,
@@ -9,9 +11,13 @@ function Car(make, model, year, engine, mileage) {
     this.mileage = mileage
 }
 
-const newCar = new Car("Hyundai", "Creta Su2id", 2025, "a combustión", "0km")
+let newCar = new Car(
+    prompt("Indique la marca del auto: "),
+    prompt("Indique el modelo del auto: "),
+    prompt("Ingrese el año de fabricación: "),
+    prompt("Indique si el motor es eléctrico o a combustión: "),
+    prompt("Indique la cantidad de kilométros de metraje: ")
+    )
 
 console.log("El auto es un " + newCar.make + " " + newCar.model + " del año " + newCar.year + ", con un motor " + newCar.engine + " y un kilometraje de " + newCar.mileage);
-
 // Type your code above this line!
-
