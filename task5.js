e0450c829d18c16e812fd7abdb910544389a8eac
Francesca Carcamo <francarcamolopez97@gmail.com>
2025-11-06 14:00:37 -0300

    feat: agrego archivo task5.js

--- task5.js
+++ task5.js
@@ -1,8 +1,26 @@
+function FriendsList() {
+  this.names = []; 
+  this.addFriend = function(name) {
+    this.names.push(name);
+  };
+
+  this.showFriends = function() {
+    console.log("Lista de amigos:", this.names);
+  };
+}
+const lista = new FriendsList();
+
+const cantidad = parseInt(prompt("¿Cuántos amigos quieres agregar? "));
+
+for (let i = 0; i < cantidad; i++) {
+  const nombre = prompt(`Ingresa el nombre del amigo ${i + 1}: `);
+  lista.addFriend(nombre);
+}
+
+lista.showFriends();
 
 
-// Type your code below this line!
 
 
 
-// Type your code above this line!
 
