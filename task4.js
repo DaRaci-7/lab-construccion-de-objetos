e412c04e6adb1c5784cca3f967b6769a8b608362
Vicente Diaz <vdiazvergara97@gmail.com>
2025-11-06 13:31:06 -0300

    feat(task4): implementa constructor Journey con método book()

--- task4.js
+++ task4.js
@@ -1,9 +1,19 @@
-// Type your code below this line!
+// Type your code below this line
+
+function Journey(start, end) {
+  this.start = start;
+  this.end = end;
+
+  this.book = function() {
+    console.log(`Booking a taxi from ${this.start} to ${this.end}`);
+  };
+}
+
+// Puedes obtener los datos desde la línea de comandos:
+const from = process.argv[3] || "Home";
+const to = process.argv[4] || "Work";
+
+const trip = new Journey(from, to);
+trip.book();
 
 
-
-// Type your code above this line!
-
-const travel = new Journey(from, to)
-
-console.log("Booking a taxi from " + travel.start + " to " + travel.end + ".")
